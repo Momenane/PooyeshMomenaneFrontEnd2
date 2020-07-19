5da155a7adddaa02126416c58d096d36bbe3fb53
ahmadTalezari <ahmad.tlz@droppgroup.com>
2020-07-19 11:17:20 +0430

    add some feature like logut and getToken

--- src/services/UserServiceRegister.js
+++ src/services/UserServiceRegister.js
@@ -1,7 +1,8 @@
 import http from './httpService';
 import config from "../config.json";
 
-const apiEndpoint = config.apiURl + "/user";
+const apiEndpoint = config.apiURl + "user";
+const tokenKey = "token";
 
 export async function register(bodyData) {
   const { data } = await http.post(
@@ -16,3 +17,18 @@ export async function register(bodyData) {
   );
   return data
 }
+
+export function logout() {
+  localStorage.removeItem(tokenKey);
+}
+
+export function getToken() {
+  return localStorage.getItem(tokenKey);
+}
+
+
+export default {
+  register,
+  logout,
+  getToken
+};
