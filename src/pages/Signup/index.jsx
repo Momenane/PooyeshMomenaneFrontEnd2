fa512ab3a211cd116790c0cbe2c021090b1e71ad
ahmadTalezari <ahmad.tlz@droppgroup.com>
2020-07-19 11:18:56 +0430

    add register service in signup for request

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,7 +1,8 @@
 import React ,{useState}from "react";
-import './index.css'
 import SignForm from "./SignForm";
+import uRegister from '../../services/UserServiceRegister'
 import signBg from '../../assets/sign-login-bg.svg'
+import './index.css'
 
 
 
@@ -34,11 +35,46 @@ export default (props) => {
     }
   };
 
+  const doSubmit = async () => {
+    const bodyData= {
+      username: signName,
+      firstName: signName,
+      lastName: signLastname,
+      password: signPassword,
+      role: "GAdmin",
+      email: signEmail,
+    }
+    
+  if(signPassword.trim() ===signRePassword.trim()){
+    try {
+      console.log('bodyData',bodyData);
+      await uRegister.register(bodyData);
+
+      const { state } = props.location;
+      window.location = state ? state.from.pathname : "/panel/poorslist";
+    } catch (ex) {
+      console.log(ex.response);
+      if (ex.response && ex.response.status === 400) {
+        console.log(ex.response);
+        // toast.error("excepted error error 400");
+      }
+    }
+  }else{
+    console.log('password and repassword not correct')
+  }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   doSubmit();
+  };
+
   return (
     <>
     <div className="row signContainer m-0 p-0" >
       <div className="col-md-6 col-12">
       <SignForm
+      onSubmit={handleSubmit}
       signName={signName}
       signLastname={signLastname}
       signZipCode={signZipCode}
