3a9ff734ef5f73602aa05e227154a4c6730a9739
ahmadTalezari <ahmad.tlz@droppgroup.com>
2020-07-23 08:24:27 +0430

    delete exact from Route edit profile and profile

--- src/pages/Panel/index.jsx
+++ src/pages/Panel/index.jsx
@@ -16,8 +16,8 @@ export default (props) => {
   return (
     <>
       <Switch>
-        <Route  exact path="/panel/editProfile" component={EditProfile} />
-        <Route  exact path="/panel/profile" component={Profile} />
+        <Route   path="/panel/editProfile" component={EditProfile} />
+        <Route   path="/panel/profile" component={Profile} />
         <Route   path="/panel/contactAdmin" component={ContactAdmin} />
         <Route  path="/panel/helps"   component={PoorsHelp} />
         <Route  path="/panel/poordetail/:id"   component={PoorsDetail} />
