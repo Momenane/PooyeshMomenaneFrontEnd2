c36b93e94b973add75e5165e71e389c3a6bc4f34
ahmadTalezari <ahmad.tlz@droppgroup.com>
2020-07-24 17:00:42 +0430

    add token in mainbanner nav for btn panel & exit

--- src/pages/Home/MainBanner/components/MainBannerNav.jsx
+++ src/pages/Home/MainBanner/components/MainBannerNav.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React ,{useState}from "react";
 import logo from "../../../../assets/mainpage-logo.svg";
 import { FaSearch } from "react-icons/fa";
 import { mainBanner } from "../../../../data.json";
-import "./MainBannerNav.css";
 import { Link } from "react-router-dom";
+import auth from '../../../../services/AuthService'
+
+import "./MainBannerNav.css";
+
 const MainBannerNav = () => {
+  const [userToken] = useState(auth.getJwt())
+
   return (
     <nav>
       <ul className="row m-0 p-0 align-items-center">
@@ -27,12 +32,31 @@ const MainBannerNav = () => {
             <FaSearch className="mainBannerIconSearche" />
             <input type="text" className="mainBannersearcheInput" />
           </div>
+          {userToken?
+           <>
+            <Link className="mainBannerSignup" to="/panel/poorslist">
+            پنل مدیریتی
+            </Link>
+            <button 
+            style={{borderRadius:5,marginRight:10,color:'#004182'}}
+             onClick={() => {
+             auth.logout();
+             window.location = "/";
+            }}> 
+            خروج
+           </button>
+          </>
+         :
+         <>
           <Link className="mainBannerSignup" to="/signup">
             ثبت نام
           </Link>
           <Link className="mainBannerlogin" to="/login">
            ورود
           </Link>
+          </>
+
+         }
         </li>
       </ul>
     </nav>
